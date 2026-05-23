e084a4732665ed86912842da96ec267d7bc8ac91
Lomans <lomanmaagr1@gmail.com>
2026-05-23 12:36:26 +0100

    vb

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Website Loaded");
+function openBox(title){
+
+    document.getElementById("popup").style.display = "flex";
+
+    document.getElementById("popup-title").innerText = title;
+}
+
+function closeBox(){
+
+    document.getElementById("popup").style.display = "none";
+}
