6c4344f3d3d9d827656ab966270577a34abbd7c4
Lomans <lomanmaagr1@gmail.com>
2026-05-23 12:57:48 +0100

    xvfg

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-function showPopup(){
+function showPopup(title){
 
     document.getElementById("popup").style.display = "flex";
 
+    document.getElementById("popup-title").innerText = title;
 }
 
 function closePopup(){
 
     document.getElementById("popup").style.display = "none";
-
 }
